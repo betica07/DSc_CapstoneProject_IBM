3e3a4a8d2439821b4028c5c8178e687a37b77f99
Beatriz_Calzadilla <betty071194@gmail.com>
2025-06-25 20:11:47 +0200

    Updated presentation files

--- 8 - Dashboard_with_Plotly_Dash_mod3.py
+++ 8 - Dashboard_with_Plotly_Dash_mod3.py
@@ -25,7 +25,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         {'label': 'All Sites', 'value': 'ALL'},
                                         {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
                                         {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
-                                        {'label': 'KSC LC 39A', 'value': 'KSC LC 39A'},
+                                        {'label': 'KSC LC 39A', 'value': 'KSC LC-39A'},
                                         {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'}
                                     ],
                                     value='ALL',
